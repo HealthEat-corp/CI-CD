44d67fbabf5e3df3074e78388d3a3534ef33c5db
JungwooSim <cscd053@gmail.com>
2021-08-08 00:38:13 +0900

    rename Dockerfile, add to command

---
+++ README.md
@@ -0,0 +1,4 @@
+# CI/CD
+
+### Repository
+docker image : https://hub.docker.com/repository/docker/cscd053/healtheat-api
--- cli.sh
+++ cli.sh
@@ -1,5 +1,11 @@
 # docker image 생성
 docker build --build-arg JAR_FILE=./api-0.0.1-SNAPSHOT.jar -t healtheat ./
 
+# docker image 생성
+docker build --build-arg JAR_FILE=./api-0.0.1-SNAPSHOT.jar -t cscd053/healtheat-api:0.1 ./
+
 # docker run
 docker run -p 8080:8080 healtheat
+
+# docker images push
+docker push cscd053/healtheat-api:0.1
